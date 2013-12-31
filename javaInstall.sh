764c50c8beb1937346e97e8e305ba4c7d1a696ea
Evan Seils <evanseils2013@gmail.com>
2013-12-31 14:43:59 -0800

    v1.1.1 - +Added option to download Eclipse IDE.

--- javaInstall.sh
+++ javaInstall.sh
@@ -11,23 +11,25 @@ echo "
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
                                                                       "
 
-echo "Type INSTALL to install the JDK for arm, type SET to set path."
+echo "Type INSTALL to install the JDK for ARM, type SET to add the JDK to the system PATH, or type IDE to download the Eclipse IDE for ARM."
 
 read ansr
 
 if [ $ansr == "INSTALL" ];
 then
-     run
+   run
 elif [ $ansr == "SET" ];
 then
-     crunch
+    export PATH=$PATH:opt/jdk1.8.0/bin
+elif [ $ansr == "IDE" ];
+then
+  sudo apt-get install eclipse-platform 
 else
-  exit
+ exit
 fi
 
-run() 
-{
-wget http://www.java.net/download/jdk8/archive/b121/binaries/jdk-8-ea-b121-linux-arm-vfp-hflt-19_dec_2013.tar.gz
+run() {
+ wget http://www.java.net/download/jdk8/archive/b121/binaries/jdk-8-ea-b121-linux-arm-vfp-hflt-19_dec_2013.tar.gz
 
 cd Downloads
 echo "Please enter your computer User Name"
@@ -37,11 +39,13 @@ read USER
 if [ /home/$USER/Downloads/jdk-8-ea-b121-linux-arm-vfp-hflt-19_dec_2013.tar.gz ]; then
     sudo tar zxvf jdk-8-ea-b121-linux-arm-vfp-hflt-19_dec_2013.tar.gz -C /opt
     export PATH=$PATH:opt/jdk1.8.0/bin
+    clear
+    echo "
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                    Installation Complete!
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 else
    echo "This only works on the December 19, 2013 build."
+   clear
 fi
-}
-crunch()
-{
-export PATH=$PATH:opt/jdk1.8.0/bin
 }
